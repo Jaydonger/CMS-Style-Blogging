54cdf1c22a4060b7cd4e6b07558bf2d6af7743ac
Jordan Pace <jordanpace95@gmail.com>
2023-03-23 22:52:46 -0600

    temp fix for infinite refresh

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,7 @@ const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 // homepage route
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const postData = await Post.findAll({
 

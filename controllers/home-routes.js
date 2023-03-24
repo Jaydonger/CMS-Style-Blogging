e253d491473d1cc051b8e231697dd85cc58aa50d
Jordan Pace <jordanpace95@gmail.com>
2023-03-23 22:37:28 -0600

    adds more code n stuf

---
+++ controllers/home-routes.js
@@ -0,0 +1,71 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+// homepage route
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+
+      include: [
+        {
+          model: User,
+          attributes: {
+            exclude: ['password'],
+          }
+        },
+        {
+          model: Comment,
+          include: [User]
+        }
+      ]
+    });
+    console.log({postData});
+    const posts = postData.map((post) =>
+      post.get({ plain: true })
+    );
+    console.log(posts);
+    console.log(posts[0].Comments[0]);
+    res.render('homepage', {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one post
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postIDdata = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [
+            User
+          ],
+        },
+      ],
+    });
+    const post = postIDdata.get({ plain: true });
+    res.render('post', { post, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+// login route
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
+module.exports = router;
---
+++ seeds/index.js
@@ -0,0 +1,40 @@
+const sequelize = require('../config/connections');
+const { User, Post, Comment} = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedAll = async () => {
+
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+});
+
+for (const post of postData) {
+
+  const posts = await Post.create({
+    ...post,
+    user_id: users[0].id,
+  });
+  console.log(posts);
+}
+
+for(const comment of commentData) {
+
+  const comments = await Comment.create({
+    ...comment,
+    post_id: users[0].id,
+    user_id: users[0].id,
+  });
+  console.log(comments);
+}
+
+  process.exit(0);
+};
+
+seedAll();
+
